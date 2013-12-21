4ad0efefa8a66dde1320cd568d02b4df4064dcce
kohachori <Sho@Shos-MacBook-Air-2.local>
2013-12-22 00:48:35 +0900

    普通の座標系になった

--- src/com/example/mmobase/MainRenderer.scala
+++ src/com/example/mmobase/MainRenderer.scala
@@ -6,14 +6,23 @@ import android.opengl.GLSurfaceView.Renderer
 
 class MainRenderer extends Renderer {
 
+  var mWidth: Int = _
+  var mHeight: Int = _
+
   override def onSurfaceCreated(gl: GL10, config: EGLConfig) {
   }
 
   override def onSurfaceChanged(gl: GL10, width: Int, height: Int) {
+    mWidth = width
+    mHeight = height
     gl.glViewport(0, 0, width, height);
   }
 
   override def onDrawFrame(gl: GL10) {
+    gl.glMatrixMode(GL10.GL_PROJECTION);
+    gl.glLoadIdentity();
+    gl.glOrthof(0, mWidth, mHeight, 0, 50f, -50f);
+
     gl.glMatrixMode(GL10.GL_MODELVIEW);
     gl.glLoadIdentity();
 
@@ -25,7 +34,7 @@ class MainRenderer extends Renderer {
   private def renderMain(gl: GL10) {
     val centerX = 0
     val centerY = 0
-    val mActuallyPlayerSize = 0.5f
+    val mActuallyPlayerSize = 100
     GraphicUtil.drawCircle(gl, centerX, centerY, 100, mActuallyPlayerSize, 1.0f, 1.0f, 1.0f, 1.0f)
   }
 }
