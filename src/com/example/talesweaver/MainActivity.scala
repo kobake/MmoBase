1fba82ebddf5dd37d215c556e87586dc4bfe0894
kobake <kobake@users.sourceforge.net>
2013-12-21 20:03:38 +0900

    GL

--- src/com/example/talesweaver/MainActivity.scala
+++ src/com/example/talesweaver/MainActivity.scala
@@ -7,7 +7,12 @@ import android.view.Menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
- 
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
 /*
 public class MainActivity extends Activity {
 
@@ -29,8 +34,17 @@ public class MainActivity extends Activity {
 
 class MainActivity extends Activity {
 	override def onCreate(savedInstanceState : Bundle) : Unit = {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+//		super.onCreate(savedInstanceState)
+//		setContentView(R.layout.activity_main)
+		
+		
+	    super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction()
+                        .replace(R.id.container_main, new MainFragment()).commit();
 	}
 
 	override def onCreateOptionsMenu(menu : Menu) : Boolean = {
