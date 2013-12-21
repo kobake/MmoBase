6ce333c68c0ceee810900422283cdfdd8067bf07
kohachori <Sho@Shos-MacBook-Air-2.local>
2013-12-22 03:44:12 +0900

    セミコロン取った

--- src/com/example/mmobase/MainRenderer.scala
+++ src/com/example/mmobase/MainRenderer.scala
@@ -1,4 +1,4 @@
-package com.example.mmobase;
+package com.example.mmobase
 
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
@@ -16,20 +16,20 @@ class MainRenderer(context: Context) extends Renderer {
   override def onSurfaceChanged(gl: GL10, width: Int, height: Int) {
     mWidth = width
     mHeight = height
-    gl.glViewport(0, 0, width, height);
+    gl.glViewport(0, 0, width, height)
     loadTextures(gl)
   }
 
   override def onDrawFrame(gl: GL10) {
-    gl.glMatrixMode(GL10.GL_PROJECTION);
-    gl.glLoadIdentity();
-    gl.glOrthof(0, mWidth, mHeight, 0, 50f, -50f);
+    gl.glMatrixMode(GL10.GL_PROJECTION)
+    gl.glLoadIdentity()
+    gl.glOrthof(0, mWidth, mHeight, 0, 50f, -50f)
 
-    gl.glMatrixMode(GL10.GL_MODELVIEW);
-    gl.glLoadIdentity();
+    gl.glMatrixMode(GL10.GL_MODELVIEW)
+    gl.glLoadIdentity()
 
-    gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+    gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+    gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
     renderMain(gl)
   }
 
@@ -41,13 +41,13 @@ class MainRenderer(context: Context) extends Renderer {
     GraphicUtil.drawTexture(gl, 0.0f, 0.0f, mWidth, mHeight, mBgTexture, 1.0f, 1.0f, 1.0f, 1.0f)
   }
 
-  private var mBgTexture: Int = _; // 背景用テクスチャ
-  private var mTargetTexture: Int = _; // 標的用テクスチャ
-  private var mNumberTexture: Int = _; // 数字用テクスチャ
+  private var mBgTexture: Int = _ // 背景用テクスチャ
+  private var mTargetTexture: Int = _ // 標的用テクスチャ
+  private var mNumberTexture: Int = _ // 数字用テクスチャ
   private def loadTextures(gl: GL10) {
-    val res = context.getResources();
-    this.mBgTexture = GraphicUtil.loadTexture(gl, res, R.drawable.ic_launcher);
-    this.mTargetTexture = GraphicUtil.loadTexture(gl, res, R.drawable.ic_launcher);
-    this.mNumberTexture = GraphicUtil.loadTexture(gl, res, R.drawable.ic_launcher);
+    val res = context.getResources()
+    this.mBgTexture = GraphicUtil.loadTexture(gl, res, R.drawable.ic_launcher)
+    this.mTargetTexture = GraphicUtil.loadTexture(gl, res, R.drawable.ic_launcher)
+    this.mNumberTexture = GraphicUtil.loadTexture(gl, res, R.drawable.ic_launcher)
   }
 }
